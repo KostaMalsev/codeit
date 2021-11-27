67b4aa071bf18e6fd46e5b625e699180da84de9a
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-11-27 15:30:22 +0200

    Update jsxp.js
    adding babel online conversion

--- externals/jsxp.js
+++ externals/jsxp.js
@@ -1,1 +1,27 @@
 /*jsx parser*/
+
+async function ParseJSX(code)
+{
+  let res = '';
+  if(! (typeof(Babel)=='undefined )
+  {
+     //res = Babel.transform(`const name = 'Josh Perez'; const element = <h1>Hello, {name}</h1>;`);, { presets: ["env", "es2015", "react"] }).code;
+     res = Babel.transform(code, { presets: ["env", "es2015", "react"] }).code;
+     
+  }else{
+    res = '<div> No babel loaded </div>';
+  }
+
+  
+}
+
+/*
+let result1 = Babel.transform(`const name = 'Josh Perez';
+  const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);`, { presets: ["env", "es2015", "react"] }).code;
+    Function(result1)(window);
+*/
