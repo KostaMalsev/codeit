ae53db1123948b44f04868ecf7b210d19f0f8b74
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-11-28 08:03:28 +0200

    Update jsxp.js

--- externals/jsxp.js
+++ externals/jsxp.js
@@ -9,13 +9,4 @@ function parseJSX(code) {
   
 }
 
-/*
-let result1 = Babel.transform(`const name = 'Josh Perez';
-  const element = <h1>Hello, {name}</h1>;
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);`, { presets: ["env", "es2015", "react"] }).code;
-    Function(result1)(window);
-*/
