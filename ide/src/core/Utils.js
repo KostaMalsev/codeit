3f9f182a3922b39db4e48b13f5ca8fab4c7d5dcf
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2025-03-11 16:10:20 +0200

    fixing decodunicode

--- ide/src/core/Utils.js
+++ ide/src/core/Utils.js
@@ -22,7 +22,7 @@ class Utils {
         if (content == undefined || content == '') return '';
 
         // going backwards: from bytestream, to percent-encoding, to original string
-        return decodeURIComponent(atob(str).split('').map(function (c) {
+        return decodeURIComponent(atob(content).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
     }
